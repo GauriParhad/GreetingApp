0a0310cf9b5589226c96c1d8ce54108c675518ae
Gaurip10 <thegauriparhad@gmail.com>
2021-10-10 18:20:21 +0530

    Added: Giving Hello World Message

--- src/main/java/cm/bridgelabz/MyGreetingApp/service/GreetingAppService.java
+++ src/main/java/cm/bridgelabz/MyGreetingApp/service/GreetingAppService.java
@@ -1,4 +1,13 @@
 package cm.bridgelabz.MyGreetingApp.service;
 
+import cm.bridgelabz.MyGreetingApp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class GreetingAppService {
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    public String getHelloMessage() {
+        return "Hello World";
+    }
 }
